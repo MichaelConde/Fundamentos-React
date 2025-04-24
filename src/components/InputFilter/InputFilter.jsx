eea0db023d3be9c5c64f38db605390dbe9718b4c
Sena <dimatesebas73@gmail.com>
2025-04-24 13:10:33 -0500

    Creacion del Buscador

---
+++ src/components/InputFilter/InputFilter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const InputFilter = () => {
+  return (
+    <input
+      type="text"
+      placeholder="Buscar producto..."
+      className="w-72 p-3 rounded-xl border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-300"
+    />
+  )
+}
