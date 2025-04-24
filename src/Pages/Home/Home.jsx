49bb63982881f35fac3b17aee542417f44677cb7
Sena <dimatesebas73@gmail.com>
2025-04-24 12:42:35 -0500

    Api linkeado

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,18 +4,18 @@ import { useState } from 'react';
 import { Card } from '../../layout/Card/Card';
 export const Home = () => {
   const [characters, setCharacters] = useState([])
-const URL = 'https://rickandmortyapi.com/api/character'
+const URL = 'https://fakestoreapi.com/products'
 
   const handleGetCharacters = async () => {
     const response = await fetch(URL)
     const data = await response.json()
-    setCharacters(data.results)
+    setCharacters(data)
   }
 
   return (
     
       <Main style="w-full h-screen flex flex-col items-center">
- <button onClick={handleGetCharacters}>Traer personajes</button>
+ <button onClick={handleGetCharacters}>Traer prductos</button>
       <section className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {
           characters.map(character => (
--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -9,13 +9,11 @@ export const Header = () => {
   return (
     <header className="top-0 left-0 z-50 w-full h-[8rem] flex-col
  bg-zinc-100 flex items-center content-center justify-center px-6 py-4 text-black shadow-md">
-  <ItemText content="Rick And Morty" style='text-7xl mb-5' />
+  <ItemText content="Productos api" style='text-7xl mb-5' />
 
 
       <Navbar style='flex justify-between'>  
-        <ItemNavbar route="/" styles="mr-15"content="Rick And Morty" />
-        <ItemNavbar route="/" styles="mr-15"content="Cats" />
-        <ItemNavbar route="/" styles="mr-15"content="Gifts" />
+        
       </Navbar>
     </header>
   );
